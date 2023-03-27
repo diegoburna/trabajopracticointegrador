7776ae710037a8b4a96a5f9e3536fe08f1b50c7e
Diego Burna <diegohburna@hotmail.com>
2023-03-26 23:20:21 -0300

    primer commit

---
+++ src/primera_entrega/Pronostico.java
@@ -0,0 +1,10 @@
+package primera_entrega;
+
+public class Pronostico {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/primera_entrega/TablaDePuntajes.java
@@ -0,0 +1,9 @@
+package primera_entrega;
+
+public class TablaDePuntajes {
+
+	public static void main(String[] args) {
+
+	}
+
+}
