5e73a5d1ebe17233083ee02b4ef907028a93cfec
diegoburna <116610007+diegoburna@users.noreply.github.com>
2023-03-29 20:37:22 -0300

    Update Ronda.java

--- src/primera_entrega/Ronda.java
+++ src/primera_entrega/Ronda.java
@@ -1,10 +1,31 @@
-package primera_entrega;
+package modelo;
 
 public class Ronda {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private Integer nroRonda;
+	private Partido partido;
+	/**
+	 * @param nroRonda
+	 * @param partido
+	 */
+	public Ronda(Integer nroRonda, Partido partido) {
+		this.nroRonda = nroRonda;
+		this.partido = partido;
 	}
-
+	/**
+	 * 
+	 */
+	public Ronda() {
+	}
+	public Integer getNroRonda() {
+		return nroRonda;
+	}
+	public void setNroRonda(Integer nroRonda) {
+		this.nroRonda = nroRonda;
+	}
+	public Partido getPartido() {
+		return partido;
+	}
+	public void setPartido(Partido partido) {
+		this.partido = partido;
+	}	
 }
