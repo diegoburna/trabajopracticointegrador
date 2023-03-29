d1c97a61189765ab774c6ceb32c68d50998cb2d0
diegoburna <116610007+diegoburna@users.noreply.github.com>
2023-03-29 20:36:20 -0300

    Update Pronostico.java

--- src/primera_entrega/Pronostico.java
+++ src/primera_entrega/Pronostico.java
@@ -1,10 +1,46 @@
-package primera_entrega;
+package modelo;
 
 public class Pronostico {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private Partido partido;
+	private Equipo equipo;
+	private ResultadoEnum resultado;
+	/**
+	 * @param partido
+	 * @param equipo
+	 * @param resultado
+	 */
+	public Pronostico(Partido partido, Equipo equipo, ResultadoEnum resultado) {
+		this.partido = partido;
+		this.equipo = equipo;
+		this.resultado = resultado;
 	}
-
+	/**
+	 * 
+	 */
+	public Pronostico() {
+		
+		
+	}
+	public Partido getPartido() {
+		return partido;
+	}
+	public void setPartido(Partido partido) {
+		this.partido = partido;
+	}
+	public Equipo getEquipo() {
+		return equipo;
+	}
+	public void setEquipo(Equipo equipo) {
+		this.equipo = equipo;
+	}
+	public ResultadoEnum getResultado() {
+		return resultado;
+	}
+	public void setResultado(ResultadoEnum resultado) {
+		this.resultado = resultado;
+	}
+	
+	
+	
+	
 }
