623dade49bb0fa6cf836ef67216482cda9ef1c98
diegoburna <116610007+diegoburna@users.noreply.github.com>
2023-03-29 20:34:58 -0300

    Update Equipo.java

--- src/primera_entrega/Equipo.java
+++ src/primera_entrega/Equipo.java
@@ -1,10 +1,32 @@
-package primera_entrega;
+package modelo;
 
 public class Equipo {
+    private String nombre;
+    private String descripcion;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public Equipo(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
 
-	}
+    public Equipo() {
+
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
 
 }
